559eb5f067462173562b3a95ea686703cefd85b0
Kevin Phung <kevinphung88@yahoo.com>
2017-05-23 22:14:43 -0700

    Added image saving for Macs (pyscreenshot)

--- imagebox.py
+++ imagebox.py
@@ -1,5 +1,6 @@
 import Tkinter as tk
-from PIL import Image, ImageTk, ImageGrab
+from PIL import Image, ImageTk      
+import pyscreenshot as ImageGrab   # install pyscreenshot because ImageGrab from PIL only works with windows 
 
 # Allow user to draw boxes in an image
 class ImageBoxer:
@@ -18,7 +19,6 @@ class ImageBoxer:
         # create a frame for listbox
         self.frame = tk.Frame(root)
         self.frame.grid(row=0,column=1)
-        #self.frame.pack(side=tk.RIGHT)
 
         self.image = tk.PhotoImage(file=r'image.gif')
 
@@ -28,18 +28,15 @@ class ImageBoxer:
         self.canvas.bind("<ButtonRelease-1>", self.canvasMouseRelease)
         self.canvas.bind("<B1-Motion>", self.canvasMouseMotion)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.image)
-        #self.canvas.pack(side=tk.LEFT)
         self.canvas.grid(row=0,column=0)
 
         # let user double-click to delete box
         self.listbox = tk.Listbox(self.frame)
         self.listbox.bind("<Double-Button-1>", self.listboxDoubleClick)
-        #self.listbox.pack(side=tk.LEFT,padx=5,pady=5)
         self.listbox.grid(row=0,column=3)
 
         # added scrollbar for listbox
         self.scrollbar = tk.Scrollbar(self.frame)
-        #self.scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
         self.scrollbar.grid(row=0,column=4,sticky="NS")
         # attach listbox to scrollbar
         self.listbox.config(yscrollcommand=self.scrollbar.set)
@@ -48,12 +45,10 @@ class ImageBoxer:
         # added undo button for rectangles
         self.undo = tk.Button(self.root,text="Undo",command=self.undoRectangle)
         self.undo.grid(row=1,column=1)
-       # self.undo.pack(side=tk.LEFT,padx=5,pady=5)
 
         # added redo button for rectangles
         self.redo = tk.Button(self.root,text="Redo",command=self.redoRectangle)
         self.redo.grid(row=1,column=2)
-        #self.redo.pack(side=tk.LEFT,padx=5,pady=5)
 
         # added entry to set image name when saving
         tk.Label(self.root,text="Image Name: ").grid(row=2,column=1)
@@ -64,7 +59,7 @@ class ImageBoxer:
         self.reset = tk.Button(self.root,text="Reset",command=self.resetAll)
         self.reset.grid(row=3,column=1)
 
-        # added save picture button (windows only)
+        # added save picture button
         self.save = tk.Button(self.root,text="Save",command=self.saveImage)
         self.save.grid(row=3,column=2)
 
@@ -143,13 +138,11 @@ class ImageBoxer:
         
     def saveImage(self):
         image = [self.canvas.canvasx(0),self.canvas.canvasy(0),self.canvas.canvasx(1),self.canvas.canvasy(1)]
-        #self.canvas.postscript(file="out.eps")
-        #image = Image.open("out.eps")
-        #image.save("out.jpg")
+
         # Get x,y position of top left of window
         window_x = self.root.winfo_x()
         window_y = self.root.winfo_y()
-        print(self.imageName)
+
         # Setup coordinates of the canvas to take screenshot
         image[0] = self.root.winfo_x()+(self.canvas.winfo_width()-self.canvas.winfo_width())+10
         image[1] = self.root.winfo_y()+(self.root.winfo_height()-self.canvas.winfo_height())
@@ -158,9 +151,6 @@ class ImageBoxer:
         image = ImageGrab.grab(bbox=(image))
         image.show()
         image.save(self.imageName.get()+".jpg")
-        #self.savedimage = ImageGrab.grab(bbox=image)
-        #self.savedimage.show()
-        #self.savedimage.save("out.jpg")
         print('Screenshoot of tkinter.Canvas saved in "'+self.imageName.get()+'.jpg"')
 
 if __name__ == "__main__":
